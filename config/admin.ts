de060654de0bfd58c9e8f2b09fde50ffcf6f603d
Nattanon <s6602041520063@email.kmutnb.ac.th>
2025-02-05 17:02:09 +0700

    up4

--- config/admin.ts
+++ config/admin.ts
@@ -1,13 +1,13 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'E0cdw1ISll9aHoy4GsQ7Xg=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'WMyAvMswUq8ckfF+1ageig=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', '291wlPuDFcyoKEqXFU4/dw=='),
     },
   },
   flags: {
--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 5432),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['9DTx/gQUQxcak2FRKrQ8eQ==', 'O8JP7s8pRsEBIOViFXHz1A==', 'dCBg4OqzVJHinqfH3Cozjg==', 'AKtSkLH9uxczFmsoD7w0Aw==']),
   },
 });
